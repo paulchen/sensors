43a94a4d72550a908675ce536da63658d9b24872
Paul Staroch <paulchen@rueckgr.at>
2012-02-20 14:12:06 +0000

    further development

--- status.php
+++ status.php
@@ -169,6 +169,7 @@ if(php_sapi_name() == 'cli') {
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="refresh" content="300; URL=.">
 <title>Sensor status</title>
 <style type="text/css">
 body { font-family: Verdana,Arial,Helvetica,sans-serif;; }
