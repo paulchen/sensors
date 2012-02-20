65e31f79369b8587c019ca47cae97b2543490e02
Paul Staroch <paulchen@rueckgr.at>
2012-02-20 13:17:22 +0000

    further development

--- status.php
+++ status.php
@@ -80,30 +80,76 @@ $stmt->execute();
 $stmt->bind_result($id, $sensor, $type, $description);
 $sensors = array();
 while($stmt->fetch()) {
+	if($description == '') {
+		$description = "Sensor $sensor";
+	}
 	$sensors[$id] = array('sensor' => $sensor, 'type' => $type, 'description' => $description);
 }
 $stmt->close();
+
+$stmt = $mysqli->prepare('SELECT sensor, value, low_crit, low_warn, high_warn, high_crit FROM sensor_limits');
+$stmt->execute();
+$stmt->bind_result($sensor, $value, $low_crit, $low_warn, $high_warn, $high_crit);
+$limits = array();
+while($stmt->fetch()) {
+	$limits["$sensor-$value"] = array('low_crit' => $low_crit, 'low_warn' => $low_warn, 'high_warn' => $high_warn, 'high_crit' => $high_crit);
+}
+$stmt->close();
+
+$states = array();
+foreach($keys as $index => $key) {
+	if(isset($limits[$index])) {
+		# TODO configurable
+		if(time()-$current_values[$index]['timestamp'] > 15*60) {
+			$states[$index] = 'UNKNOWN (most recent value is too old)';
+		}
+		else {
+			$value = $current_values[$index]['value'];
+			if($value < $limits[$index]['low_crit']) {
+				$states[$index] = 'CRITICAL (below limit of ' . str_replace('%s', round($limits[$index]['low_crit'], $values[$key['what']]['decimals']), $values[$key]['what']['format']) . ')';
+			}
+			else if($value < $limits[$index]['low_warn']) {
+				$states[$index] = 'WARNING (below limit of ' . str_replace('%s', round($limits[$index]['low_warn'], $values[$key['what']]['decimals']), $values[$key]['what']['format']) . ')';
+			}
+			else if($value > $limits[$index]['high_crit']) {
+				$states[$index] = 'CRITICAL (above limit of ' . str_replace('%s', round($limits[$index]['high_crit'], $values[$key['what']]['decimals']), $values[$key]['what']['format']) . ')';
+			}
+			else if($value > $limits[$index]['high_warn']) {
+				$states[$index] = 'WARNING (above limit of ' . str_replace('%s', round($limits[$index]['high_warn'], $values[$key['what']]['decimals']), $values[$key]['what']['format']) . ')';
+			}
+			else {
+				$states[$index] = 'OK';
+			}
+		}
+	}
+	else {
+		$states[$index] = 'UNKNOWN (no limits set)';
+	}
+}
+
+foreach($keys as $index => $key) {
+	$current_values[$index]['formatted_value'] = str_replace('%s', round($current_values[$index]['value'], $values[$what]['decimals']), $values[$what]['format']);
+	$current_values[$index]['formatted_timestamp'] = date('Y-m-d H:i', $current_values[$index]['timestamp']);
+
+	$min_values[$index]['formatted_value'] = str_replace('%s', round($min_values[$index]['value'], $values[$what]['decimals']), $values[$what]['format']);
+	$min_values[$index]['formatted_timestamp'] = date('Y-m-d H:i', $min_values[$index]['timestamp']);
+
+	$max_values[$index]['formatted_value'] = str_replace('%s', round($max_values[$index]['value'], $values[$what]['decimals']), $values[$what]['format']);
+	$max_values[$index]['formatted_timestamp'] = date('Y-m-d H:i', $max_values[$index]['timestamp']);
+}
 
 if(php_sapi_name() == 'cli') {
 	foreach($keys as $index => $key) {
 		$sensor = $key['sensor'];
 		$what = $key['what'];
 
-		# TODO sensor description
-		echo "\n\n";
-		if($sensors[$sensor]['description'] == '') {
-			echo "Sensor $sensor";
-		}
-		else {
-			echo $sensors[$sensor]['description'];
-		}
-		echo " - " . $values[$what]['name'] . ":\n\n";
-		# TODO format
-		# TODO current state (ok/warning/critical)
-		echo "Current value: " . round($current_values[$index]['value'], $values[$what]['decimals']) . " (" . date('Y-m-d H:i', $current_values[$index]['timestamp']) . ")\n";
-		echo "Maximum value (24 hours): " . round($max_values[$index]['value'], $values[$what]['decimals']) . " (" . date('Y-m-d H:i', $max_values[$index]['timestamp']) . ")\n";
-		echo "Minimum value (24 hours): " . round($min_values[$index]['value'], $values[$what]['decimals']) . " (" . date('Y-m-d H:i', $min_values[$index]['timestamp']) . ")\n";
+		echo $sensors[$sensor]['description'] . " - " . $values[$what]['name'] . ":\n\n";
+		echo "Current state: " . $states[$index] . "\n";
+		echo "Current value: " . $current_values[$index]['formatted_value'] . " (" . $current_values[$index]['formatted_timestamp'] . ")\n";
+		echo "Maximum value (24 hours): " . $min_values[$index]['formatted_value'] . " (" . $min_values[$index]['formatted_timestamp'] . ")\n";
+		echo "Minimum value (24 hours): " . $max_values[$index]['formatted_value'] . " (" . $max_values[$index]['formatted_timestamp'] . ")\n";
 		echo "Current tendency: " . $tendencies[$index] . "\n";
+		echo "\n\n";
 	}
 
 	exit;
