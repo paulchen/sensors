97c508502929c14f4a6da291fcc905ac3768a922
Paul Staroch <paulchen@rueckgr.at>
2012-02-21 11:07:25 +0000

    XHTML 1.1 compliance

--- status.php
+++ status.php
@@ -191,12 +191,12 @@ if(php_sapi_name() == 'cli') {
 	exit;
 }
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+    "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta http-equiv="refresh" content="300; URL=.">
+<meta http-equiv="refresh" content="300; URL=." />
 <title>Sensor status</title>
 <style type="text/css">
 body { font-family: Verdana,Arial,Helvetica,sans-serif;; }
