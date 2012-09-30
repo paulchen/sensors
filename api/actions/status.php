efc72d272435b862e316424d048afd5be95da764
Paul Staroch <paulchen@rueckgr.at>
2012-09-30 13:33:37 +0000

    further API development

--- api/actions/status.php
+++ api/actions/status.php
@@ -5,6 +5,8 @@ if(basename($_SERVER['SCRIPT_FILENAME']) != 'index.php') {
 }
 
 $sensors = get_sensors();
+$sensor_ids = array_map(function($value) { return $value['id']; }, $sensors);
+$sensor_data = get_sensors_state($sensor_ids);
 
 $success = true;
 
--- api/common.php
+++ api/common.php
@@ -42,3 +42,46 @@ function get_sensors() {
 	return $sensors;
 }
 
+function get_sensors_state($sensors = array()) {
+	global $mysqli;
+
+	if(!is_array($sensors)) {
+		// TODO
+		die();
+	}
+
+	$question_marks = str_repeat('?, ', count($sensors)-1) . '?';
+	$query = 'SELECT sensor, what, UNIX_TIMESTAMP(timestamp) timestamp, value FROM sensor_data WHERE timestamp > ? AND sensor IN (' . $question_marks . ') ORDER BY sensor ASC, what ASC';
+	$stmt = $mysqli->prepare($query);
+	$args = array(str_repeat('s', count($sensors)+1), &$start_timestamp);
+	foreach($sensors as $sensor) {
+		$var = $sensor;
+		$args[] = &$var;
+		unset($var);
+	}
+	$start_timestamp = date('Y-m-d H:i', time()-86400);
+	$ref = new ReflectionClass('mysqli_stmt');
+	$method = $ref->getMethod('bind_param');
+	$method->invokeArgs($stmt, $args);
+	$stmt->execute();
+	$stmt->bind_result($sensor_id, $what, $timestamp, $value);
+	$sensor_data = array();
+	while($stmt->fetch()) {
+		if(!isset($sensor_data[$sensor_id])) {
+			$sensor_data[$sensor_id] = array('values' => array());
+		}
+		if(!isset($sensor_data[$sensor_id]['values'][$what])) {
+			$sensor_data[$sensor_id]['values'][$what] = array('value' => $value, 'timestamp' => $timestamp);
+		}
+		else {
+			$old_timestamp = $sensor_data[$sensor_id]['values'][$what]['timestamp'];
+			if($old_timestamp > $timestamp) {
+				$sensor_data[$sensor_id]['values'][$what] = array('value' => $value, 'timestamp' => $timestamp);
+			}
+		}
+	}
+	$stmt->close();
+
+	return $sensor_data;
+}
+
