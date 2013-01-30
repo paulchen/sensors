8cd601d4c74e80a9f49430a5e08557a1b503a2f5
paulchen <paulchen@rueckgr.at>
2013-01-30 23:47:08 +0100

    refactoring

--- munin.php
+++ munin.php
@@ -1,21 +1,13 @@
 #!/usr/bin/php
 <?php
 
-$config_file = dirname(__FILE__) . '/config.properties';
+chdir(dirname(__FILE__));
+$config_file = 'config.properties';
 if(isset($_ENV['config_dir'])) {
 	$config_file = $_ENV['config_dir'] . '/config.properties';
 }
-$config = parse_ini_file($config_file);
-if(!$config) {
-	echo "Could not read configuration file.\n";
-	die(3);
-}
 
-$mysqli = new mysqli($config['db_host'], $config['db_username'], $config['db_password'], $config['db_database']);
-if($mysqli->connect_errno) {
-	echo "Could not connect to the database.\n";
-	die(3);
-}
+require_once('common.php');
 
 $parts = explode('_', $argv[0]);
 if(count($parts) != 3) {
@@ -25,31 +17,35 @@ if(count($parts) != 3) {
 $value = $parts[1];
 $sensor_list = $parts[2];
 
-$stmt = $mysqli->prepare('SELECT id, name, unit, min, max, decimals FROM sensor_values WHERE short = ?');
-$stmt->bind_param('s', $value);
-$stmt->execute();
-$stmt->bind_result($value_id, $value_name, $value_unit, $value_min, $value_max, $value_decimals);
-if(!$stmt->fetch()) {
+$query = 'SELECT id, name, unit, min, max, decimals FROM sensor_values WHERE short = ?';
+$data = db_query($query, array($value));
+if(count($data) != 1) {
 	echo "Invalid sensor value specified.\n";
 	die(3);
 }
-$stmt->close();
+
+$value_id = $data[0]['id'];
+$value_name = $data[0]['name'];
+$value_unit = $data[0]['unit'];
+$value_min = $data[0]['min'];
+$value_max = $data[0]['max'];
+$value_decimals = $data[0]['decimals'];
 
 $sensors = explode('.', $sensor_list);
 $sensor_info = array();
 
-$stmt = $mysqli->prepare('SELECT sensor, type, description FROM sensors WHERE id = ? ORDER BY id DESC LIMIT 0, 1');
+$query = 'SELECT sensor, type, description FROM sensors WHERE id = ? ORDER BY id DESC LIMIT 0, 1';
 foreach($sensors as $sensor_id) {
-	$stmt->bind_param('i', $sensor_id);
-	$stmt->execute();
-	$stmt->bind_result($sensor, $type, $description);
-	if(!$stmt->fetch()) {
+	$data = db_query($query, array($sensor_id));
+	if(count($data) != 1) {
 		echo "Unknown sensor ID: $sensor_id\n";
 		die(3);
 	}
-	$sensor_info[$sensor] = array('id' => $sensor_id, 'sensor' => $sensor, 'type' => $type, 'description' => $description);
+
+	$row = $data[0];
+	$row['id'] = $sensor_id;
+	$sensor_info[$row['sensor']] = $row;
 }
-$stmt->close();
 
 if(isset($argv[1]) && $argv[1] == 'config') {
 	$title = 'Sensor';
@@ -72,33 +68,35 @@ if(isset($argv[1]) && $argv[1] == 'config') {
 	}
 	echo "graph_scale no\n";
 	echo "graph_category sensor_data\n";
-	$stmt = $mysqli->prepare('SELECT low_crit, low_warn, high_warn, high_crit FROM sensor_limits WHERE sensor = ? AND value = ?');
+	$query = 'SELECT low_crit, low_warn, high_warn, high_crit FROM sensor_limits WHERE sensor = ? AND value = ?';
 	foreach($sensor_info as $index => $sensor) {
 		echo 'sensor' . $sensor['id'] . '.label ' . ($sensor['description'] != '' ? $sensor['description'] : "Sensor $index"). "\n";
-		$stmt->bind_param('ii', $sensor['id'], $value_id);
-		$stmt->execute();
-		$stmt->bind_result($low_crit, $low_warn, $high_warn, $high_crit);
-		if($stmt->fetch()) {
+		$data = db_query($query, array($sensor['id'], $value_id));
+		if(count($data) > 0) {
+			$low_crit = $data[0]['low_crit'];
+			$low_warn = $data[0]['low_warn'];
+			$high_warn = $data[0]['high_warn'];
+			$high_crit = $data[0]['high_crit'];
+
 			echo 'sensor' . $sensor['id'] . ".warning $low_warn:$high_warn\n";
 			echo 'sensor' . $sensor['id'] . ".critical $low_crit:$high_crit\n";
 		}
 	}
-	$stmt->close();
 
 	exit;
 }
 
-$stmt = $mysqli->prepare('SELECT UNIX_TIMESTAMP(timestamp) timestamp, value FROM sensor_data WHERE sensor = ? AND what = ? ORDER BY id DESC LIMIT 0, 1');
+$query = 'SELECT UNIX_TIMESTAMP(timestamp) timestamp, value FROM sensor_data WHERE sensor = ? AND what = ? ORDER BY id DESC LIMIT 0, 1';
 foreach($sensor_info as $index => $sensor) {
-	$stmt->bind_param('ii', $sensor['id'], $value_id);
-	$stmt->execute();
-	$stmt->bind_result($timestamp, $value);
-	if($stmt->fetch()) {
+	$data = db_query($query, array($sensor['id'], $value_id));
+	if(count($data) > 0) {
+		$timestamp = intval($data[0]['timestamp']);
+		$value = floatval($data[0]['value']);
+
 		if(time()-$timestamp < $config['value_outdated_period']) {
 			$value = round($value, $value_decimals);
 			echo 'sensor' . $sensor['id'] . ".value $value\n";
 		}
 	}
 }
-$stmt->close();
 
