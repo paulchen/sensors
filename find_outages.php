91fa7d2cb1b0fb54854d18985335ed4af26dfbc1
paulchen <paulchen@rueckgr.at>
2013-01-30 23:38:47 +0100

    refactoring#

--- find_outages.php
+++ find_outages.php
@@ -4,41 +4,39 @@
 chdir(dirname(__FILE__));
 require_once('common.php');
 
-$stmt = $mysqli->prepare('SELECT id, name FROM sensor_values');
-$stmt->execute();
-$stmt->bind_result($id, $name);
+$query = 'SELECT id, name FROM sensor_values';
+$data = db_query($query);
 $values = array();
-while($stmt->fetch()) {
-	$values[$id] = $name;
+foreach($data as $row) {
+	$values[$row['id']] = $row['name'];
 }
-$stmt->close();
 
-$stmt = $mysqli->prepare('SELECT id, sensor, description FROM sensors');
-$stmt->execute();
-$stmt->bind_result($id, $sensor, $description);
-$sensors = array();
-while($stmt->fetch()) {
+$query = 'SELECT id, sensor, description FROM sensors';
+$data = db_query($query);
+foreach($data as $row) {
+	$sensor = $row['sensor'];
+	$description = $row['description'];
+
 	if($description == '') {
 		$description = "Sensor $sensor";
 	}
-	$sensors[$id] = $description;
+	$sensors[$row['id']] = $description;
 }
-$stmt->close();
 
-$stmt = $mysqli->prepare('SELECT sensor, what FROM sensor_data GROUP BY sensor, what');
-$stmt->execute();
-$stmt->store_result();
-$stmt->bind_result($sensor, $what);
-while($stmt->fetch()) {
+$query = 'SELECT sensor, what FROM sensor_data GROUP BY sensor, what';
+$data = db_query($query);
+foreach($data as $row) {
+	$sensor = $row['sensor'];
+	$what = $row['what'];
+
 	echo $sensors[$sensor] . ', ' . $values[$what] . "\n";
 
-	$stmt2 = $mysqli->prepare('SELECT UNIX_TIMESTAMP(timestamp) timestamp FROM sensor_data WHERE sensor = ? AND what = ? ORDER BY id ASC');
-	$stmt2->bind_param('ii', $sensor, $what);
-	$stmt2->execute();
-	$stmt2->bind_result($timestamp);
-	$stmt2->fetch();
-	$previous_timestamp = $timestamp;
-	while($stmt2->fetch()) {
+	$query2 = 'SELECT UNIX_TIMESTAMP(timestamp) timestamp FROM sensor_data WHERE sensor = ? AND what = ? ORDER BY id ASC';
+	$data2 = db_query($query2, array($sensor, $what));
+	$previous_timestamp = intval($data2[0]['timestamp']);
+	for($a=1; $a<count($data2); $a++) {
+		$timestamp = intval($data2[$a]['timestamp']);
+
 		if($timestamp-$previous_timestamp > $config['outage_period']) {
 			echo 'Outage from ';
 			echo date('Y-m-d H:i', $previous_timestamp);
@@ -49,9 +47,7 @@ while($stmt->fetch()) {
 
 		$previous_timestamp = $timestamp;
 	}
-	$stmt2->close();
 
 	echo "\n";
 }
-$stmt->close();
 
